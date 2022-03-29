e9434d2bd5340a881a120961b71edc9b272503ed
eryk <123>
2022-03-29 10:15:09 +0200

    add request

---
+++ req/initSigned.js
@@ -0,0 +1,37 @@
+const https = require('https');
+const fs = require('fs');
+const getSigned = require('../security/sign.js')
+
+
+const options = {
+    hostname: 'ksef-test.mf.gov.pl',
+    path: '/api/online/Session/InitSigned',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/octet-stream',
+        'accept': 'application/json'
+    }
+}
+      
+
+module.exports = function initSigned (doc)  {
+
+    getSigned(doc);
+    let data = fs.readFileSync('./doc/initSigned.xml') ;
+
+    const req = https.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+    
+        res.on('data', d => {
+            
+            console.log(d);
+            process.stdout.write(d)
+            });
+        })
+        req.on('error', error => {
+            console.error(error);
+        });    
+        req.write(data);
+        req.end();
+        return "/authChallange2 done";
+}
--- security/sign.js
+++ security/sign.js
@@ -1,218 +1,70 @@
-const xadesjs = require('xadesjs');
-const { Crypto } = require('@peculiar/webcrypto');
-
-const fs = require('fs');
-const { Certificate } = require('crypto');
-
-xadesjs.Application.setEngine("NodeJS", new Crypto());
-console.log('lol');
-
-
-module.exports = function letSigned(doc){
-        
-    let myPrivateKey, myPublicKey;
-    xadesjs.Application.crypto.subtle.generateKey({
-        name: "RSASSA-PKCS1-v1_5",
-        modulusLength: 1024,
-        publicExponent: new Uint8Array([ 1, 0 , 1]),
-        hash: { name: 'SHA-256'}
-    },false,
-    ['sign' , 'verify']
-    ).then(function (keyPair) {
-        myPrivateKey = keyPair.privateKey;
-        myPublicKey = keyPair.publicKey;
-        console.log("Stworzono klucz");   
-        let xmlString = doc;
-    return SignXml(xmlString, keyPair, {name: 'RSASSA-PKCS1-v1_5', hash: { name: "SHA-256" } })})
-        .then(function (signedDocument) {
-            console.log("Signed document:\n\n", signedDocument);
-           
-
-        })
-        .catch(function (e) {
-            console.error(e);
-        });
-    }
-
-    function SignXml(xmlFileContent, keys, algorithm) {
-     return Promise.resolve()
-        .then( () => {
-            var xmlDoc = xadesjs.Parse(xmlFileContent);
-            let signedXml = new xadesjs.SignedXml();
-
-            return signedXml.Sign(
-                algorithm,
-                keys.privateKey,
-                xmlDoc,{
-                    keyValue: keys.publicKey,
-                    references: [
-                        { hash: "SHA-256", transforms: ["enveloped"] }
-                    ],
-                
-               })
-    })
-            .then(signature => signature.toString());
-    }
-
-
-/*const xadesjs = require('xadesjs');
-const { Crypto } = require('@peculiar/webcrypto');
-
-const fs = require('fs');
-const { Signature } = require('xmldsigjs');
-
-xadesjs.Application.setEngine("NodeJS", new Crypto());
-console.log('lol');
-
-module.exports = function createKey() {
-    
-        let myPrivateKey, myPublicKey;
-        xadesjs.Application.crypto.subtle.generateKey({
-            name: "RSASSA-PKCS1-v1_5",
-            modulusLength: 1024,
-            publicExponent: new Uint8Array([ 1, 0 , 1]),
-            hash: { name: 'SHA-256'}
-        },false,
-        ['sign' , 'verify']
-        ).then(function (keyPair) {
-            myPrivateKey = keyPair.privateKey;
-            myPublicKey = keyPair.publicKey;
-            console.log("Stworzono klucz");
-        return keyPair;
-          
-        })
-            .catch(function (e) {
-                console.error(e);
-            });
-        
-    
-   
-}
-
-
-module.exports = function SignXml(xmlFileContent, keys, algorithm) {
-    return Promise.resolve()
-        .then( () => {
-            var xmlDoc = xadesjs.Parse(xmlFileContent);
-            let signedXml = new xadesjs.SignedXml();
-
-            return signedXml.Sign(
-                algorithm,
-                keys.privateKey,
-                xmlDoc,{
-                    keyValue: keys.publicKey,
-                    references: [
-                        { hash: "SHA-256", transforms: ["enveloped"] }
-                    ],
-                })
-            })
-            .then(function(){
-            signature = toString(signature);
-            console.log(signature);
-            });
-            
-        }
-
-
-
-SignedXml.Sign(algorithm: Algorithm, key: CryptoKey, data: Document, options?: OptionsXAdES): PromiseLike<Signature>;
-
-
-var xadesjs = require("xadesjs");
+const fs = require("fs");
 var { Crypto } = require("@peculiar/webcrypto");
-const fs = require('fs');
+const xadesjs = require("xadesjs");
+const { XMLSerializer } = require("xmldom");
+
+const crypto = new Crypto();
 
 xadesjs.Application.setEngine("NodeJS", new Crypto());
 
-// Generate RSA key pair
-module.exports = function sign(){
-    var privateKey, publicKey;
-    xadesjs.Application.crypto.subtle.generateKey(
-    {
-        name: "RSASSA-PKCS1-v1_5",
-        modulusLength: 1024, //can be 1024, 2048, or 4096,
-        publicExponent: new Uint8Array([1, 0, 1]),
-        hash: { name: "SHA-1" }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-    },
-    false, //whether the key is extractable (i.e. can be used in exportKey)
-    ["sign", "verify"] //can be any combination of "sign" and "verify"
-)
-    .then(function (keyPair) {
-        // Push ganerated keys to global variable
-        privateKey = keyPair.privateKey;
-        publicKey = keyPair.publicKey;
-
-        // Call sign function
-        var xmlString = toString(fs.readFileSync("./doc/initSessionSign.xml"));
-        console.log(xmlString)
-        return SignXml(xmlString, keyPair, { name: "RSASSA-PKCS1-v1_5", hash: { name: "SHA-1" } });
-    })
-    .then(function (signedDocument) {
-        console.log("Signed document:\n\n", signedDocument);
-    })
-    .catch(function (e) {
-        console.error(e);
-    });
-
+function preparePem(pem) {
+    return pem
+        // remove BEGIN/END
+        .replace(/-----(BEGIN|END)[\w\d\s]+-----/g, "")
+        // remove \r, \n
+        .replace(/[\r\n]/g, "");
 }
 
-    
-function SignXml(xmlString, keys, algorithm) {
-    return Promise.resolve()
-        .then(() => {
-            var xmlDoc = xadesjs.Parse(xmlString);
-            var signedXml = new xadesjs.SignedXml();
-
-            return signedXml.Sign(               // Signing document
-                algorithm,                              // algorithm https://4programmers.net/Forum/Nietuzinkowe_tematy/355933-ksef_krajowy_system_e_faktur?page=12
-                keys.privateKey,                        // key
-                xmlDoc,                                 // document
-                {                                       // options
-                    keyValue: keys.publicKey,
-                    references: [
-                        { hash: "SHA-256", transforms: ["enveloped"] }
-                    ],
-                    productionPlace: {
-                        country: "Country",
-                        state: "State",
-                        city: "City",
-                        code: "Code",
-                    },
-                    signingCertificate: "MIIGgTCCBGmgAwIBAgIUeaHFHm5f58zYv20JfspVJ3hossYwDQYJKoZIhvcNAQEFBQAwgZIxCzAJBgNVBAYTAk5MMSAwHgYDVQQKExdRdW9WYWRpcyBUcnVzdGxpbmsgQi5WLjEoMCYGA1UECxMfSXNzdWluZyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTE3MDUGA1UEAxMuUXVvVmFkaXMgRVUgSXNzdWluZyBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eSBHMjAeFw0xMzEwMzAxMjI3MTFaFw0xNjEwMzAxMjI3MTFaMHoxCzAJBgNVBAYTAkJFMRAwDgYDVQQIEwdCcnVzc2VsMRIwEAYDVQQHEwlFdHRlcmJlZWsxHDAaBgNVBAoTE0V1cm9wZWFuIENvbW1pc3Npb24xFDASBgNVBAsTC0luZm9ybWF0aWNzMREwDwYDVQQDDAhFQ19ESUdJVDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAJgkkqvJmZaknQC7c6H6LEr3dGtQ5IfOB3HAZZxOZbb8tdM1KMTO3sAifJC5HNFeIWd0727uZj+V5kBrUv36zEs+VxiN1yJBmcJznX4J2TCyPfLk2NRELGu65VwrK2Whp8cLLANc+6pQn/5wKh23ehZm21mLXcicZ8whksUGb/h8p6NDe1cElD6veNc9CwwK2QT0G0mQiEYchqjJkqyY8HEak8t+CbIC4Rrhyxh3HI1fCK0WKS9JjbPQFbvGmfpBZuLPYZYzP4UXIqfBVYctyodcSAnSfmy6tySMqpVSRhjRn4KP0EfHlq7Ec+H3nwuqxd0M4vTJlZm+XwYJBzEFzFsCAwEAAaOCAeQwggHgMFgGA1UdIARRME8wCAYGBACLMAECMEMGCisGAQQBvlgBgxAwNTAzBggrBgEFBQcCARYnaHR0cDovL3d3dy5xdW92YWRpc2dsb2JhbC5ubC9kb2N1bWVudGVuMCQGCCsGAQUFBwEDBBgwFjAKBggrBgEFBQcLAjAIBgYEAI5GAQEwdAYIKwYBBQUHAQEEaDBmMCoGCCsGAQUFBzABhh5odHRwOi8vb2NzcC5xdW92YWRpc2dsb2JhbC5jb20wOAYIKwYBBQUHMAKGLGh0dHA6Ly90cnVzdC5xdW92YWRpc2dsb2JhbC5jb20vcXZldWNhZzIuY3J0MEYGCiqGSIb3LwEBCQEEODA2AgEBhjFodHRwOi8vdHNhMDEucXVvdmFkaXNnbG9iYWwuY29tL1RTUy9IdHRwVHNwU2VydmVyMBMGCiqGSIb3LwEBCQIEBTADAgEBMA4GA1UdDwEB/wQEAwIGQDAfBgNVHSMEGDAWgBTg+A751LXyf0kjtsN5x6M1H4Z6iDA7BgNVHR8ENDAyMDCgLqAshipodHRwOi8vY3JsLnF1b3ZhZGlzZ2xvYmFsLmNvbS9xdmV1Y2FnMi5jcmwwHQYDVR0OBBYEFDc3hgIFJTDamDEeQczI7Lot4uaVMA0GCSqGSIb3DQEBBQUAA4ICAQAZ8EZ48RgPimWY6s4LjZf0M2MfVJmNh06Jzmf6fzwYtDtQLKzIDk8ZtosqYpNNBoZIFICMZguGRAP3kuxWvwANmrb5HqyCzXThZVPJTmKEzZNhsDtKu1almYBszqX1UV7IgZp+jBZ7FyXzXrXyF1tzXQxHGobDV3AEE8vdzEZtwDGpZJPnEPCBzifdY+lrrL2rDBjbv0VeildgOP1SIlL7dh1O9f0T6T4ioS6uSdMt6b/OWjqHadsSpKry0A6pqfOqJWAhDiueqgVB7vus6o6sSmfG4SW9EWW+BEZ510HjlQU/JL3PPmf+Xs8s00sm77LJ/T/1hMUuGp6TtDsJe+pPBpCYvpm6xu9GL20CsArFWUeQ2MSnE1jsrb00UniCKslcM63pU7I0VcnWMJQSNY28OmnFESPK6s6zqoN0ZMLhwCVnahi6pouBwTb10M9/Anla9xOT42qxiLr14S2lHy18aLiBSQ4zJKNLqKvIrkjewSfW+00VLBYbPTmtrHpZUWiCGiRS2SviuEmPVbdWvsBUaq7OMLIfBD4nin1FlmYnaG9TVmWkwVYDsFmQepwPDqjPs4efAxzkgUFHWn0gQFbqxRocKrCsOvCDHOHORA97UWcThmgvr0Jl7ipvP4Px//tRp08blfy4GMzYls5WF8f6JaMrNGmpfPasd9NbpBNp7A=="
-                })
-            })
-            .then(signature => signature.toString());
+function pem2der(pem) {
+    pem = preparePem(pem);
+    // convert base64 to ArrayBuffer
+    return new Uint8Array(Buffer.from(pem, "base64")).buffer;
 }
 
-"use strict";
-
-let signature = new XmlDSigJs.SignedXml();
-
-signature.Sign(                                  // Signing document
-    { name: "RSASSA-PKCS1-v1_5" },                        // algorithm 
-    keys.privateKey,                                      // key 
-    XmlDSigJs.Parse(xml),                                 // document
-    {                                                     // options
-        keyValue: keys.publicKey,
-        references: [
-            { hash: "SHA-512", transforms: ["enveloped", "c14n"] },
-        ]
-    })
-    .then(() => {
-        console.log(signature.toString());       // <xml> document with signature
-    })
-    .catch(e => console.log(e));
-
+module.exports =   
+async function getSigned(doc) {
+        const hash = "SHA-256"
+        const alg = {
+            name: "RSASSA-PKCS1-v1_5",
+            hash,
+        }
     
-var SignedXml = require('xml-crypto').SignedXml	  
-, fs = require('fs');
-require( 'xml-crypto' ).SignedXml.enableHMAC();
+    const certPem = fs.readFileSync("./keys/myCert.crt", { encoding: "utf8" });
+    const certDer = pem2der(certPem);
+    
+    const keyPem = fs.readFileSync("./keys/myPrivateKey.pem", { encoding: "utf8" });
+    const keyDer = pem2der(keyPem);
+    const key = await crypto.subtle.importKey("pkcs8", keyDer, alg, false, ["sign"]);
+    
+    let xmlString = doc;
+    var xml = xadesjs.Parse(xmlString);
 
-var xml = fs.readFileSync("../doc/initSessionSigned.xml");
+    let xadesXml = new xadesjs.SignedXml();
+    const x509 = preparePem(certPem);
+    const signature = await xadesXml.Sign(
+        alg,                                    // algorithm
+        key,                                    // key
+        xml ,                                    // document
+        {    
+            references: [
+                { hash, transforms: ["c14n", "enveloped"] }
+            ],
+            
+            signingCertificate: x509
+        })
+          
 
-var sig = new SignedXml()
-sig.addReference("//*[local-name(.)='book']")    
-sig.signingKey = fs.readFileSync("../keys/openssl/certificate.crt")
-sig.computeSignature(xml)
-fs.writeFileSync("signed.xml", sig.getSignedXml())
-/*/
+                
+            xml.documentElement.appendChild(signature.GetXml());
+
+            // serialize XML
+            const oSerializer = new XMLSerializer();
+            const sXML = oSerializer.serializeToString(xml);
+            console.log(sXML.toString());
+            fs.writeFileSync('./doc/initSigned.xml', sXML, (err) => {
+                console.log(err);
+            });
+          
+}
+        
+ 
